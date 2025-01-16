89a4f3f1871f2bbdebb5ce389bc48072bd024183
sarahapilad0 <sarahapilado@hotmail.co.uk>
2025-01-16 01:53:22 +0000

    Create MS Access Queries.sql

---
+++ SQL Queries/MS Access Queries.sql
@@ -0,0 +1,3 @@
+-- -- SQL Queries for MS Access.
+-- This file contains a list of SQL queries designed to interact with the hospital database within this project. The queries are intended for use in MS Access.
+
